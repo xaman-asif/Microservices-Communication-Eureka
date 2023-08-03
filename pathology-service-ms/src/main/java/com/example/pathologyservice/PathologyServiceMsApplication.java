83e8dffdf3d4665146629917cae1eea3318c7d84
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-08-03 18:07:25 +0600

    Published Clients to Eureka Server

--- pathology-service-ms/src/main/java/com/example/pathologyservice/PathologyServiceMsApplication.java
+++ pathology-service-ms/src/main/java/com/example/pathologyservice/PathologyServiceMsApplication.java
@@ -2,8 +2,10 @@ package com.example.pathologyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PathologyServiceMsApplication {
 
 	public static void main(String[] args) {
